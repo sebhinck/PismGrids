af9b30bc9b9870dde9a47bab40465112c1863eed
Sebastian Hinck <sebastian.hinck@awi.de>
2018-11-29 15:00:28 +0100

    Added note about ollie nodes running out of memory

--- make_all_grid_combos.sh
+++ make_all_grid_combos.sh
@@ -20,8 +20,17 @@ for dom in nhem greenland greenland_handbook laurentide; do
 done
 
 for dom in antarctica; do
-      for res in 1 2 4 8 16 32; do
+      for res in 4 8 16 32; do
               ./run_create_domain_grid.sh $dom $res ${POOL_DIR_pism}/grids
       done
 done
 
+echo "To create antarctica grids at 1 and 2 km resolution,"
+echo "the ollie compute nodes run out of memory."
+echo "You might need to ask for more memory or use a fat node."
+echo
+echo "Then run"
+echo "   ./run_create_domain_grid.sh antarctica 1 ${POOL_DIR_pism}/grids"
+echo "and"
+echo "   ./run_create_domain_grid.sh antarctica 2 ${POOL_DIR_pism}/grids"
+echo
